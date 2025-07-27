12662db5711af1ac0d7f10ab64409babf7e7e9fb
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-07-27 23:30:18 +0530

    Create MinAndMax.java

---
+++ MinAndMax.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class MinAndMax {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter the Size of the Array: ");
+       int size = sc.nextInt();
+       int[] arr = new int[size];
+       for(int i=0;i<size;i++){
+           arr[i] = sc.nextInt();
+       }
+       int max = Integer.MIN_VALUE;
+       int min = Integer.MAX_VALUE;
+       for(int i=0;i<size;i++){
+           if(arr[i] > max){
+               max = arr[i];
+           }else if(arr[i] < min){
+               min = arr[i];
+           }
+       }
+       System.out.println("Maximum: "+max);
+       System.out.println("Minimum: "+min);
+    }
+}
