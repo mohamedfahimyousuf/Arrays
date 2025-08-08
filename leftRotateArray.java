cd403e33beabdd00c2897c1b220e5df7b79b4ac5
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-08-08 22:54:36 +0530

    Create leftRotateArray.java

---
+++ leftRotateArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class leftRotateArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int temp = arr[0];
+        for(int i=1;i<n;i++){
+            arr[i-1] = arr[i];
+        }
+        arr[n-1] = temp;
+        System.out.println("Left Rotate of the Array by one");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
